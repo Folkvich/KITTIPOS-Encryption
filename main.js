e23ce3bad8a96dcccd9308d74d5f3a0b9b8ae72a
DoMenZy <hokagenaruto112233@gmail.com>
2022-05-13 03:03:49 +0700

    edit monoapl 13/5/65

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const express = require('express');
-
+const port = 3333;
 const app = express();
 app.use(express.json());
 
-app.listen(3000 , () => {
-    console.log('Listening on port: 3000');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/main.html")
+});
+
+app.get('/MonoalphabeticCipher', (req, res) => {
+    res.sendFile(__dirname + "/MonoalphabeticCipher.html")
+});
+
+app.get('/CeacarsCipher', (req, res) => {
+    res.sendFile(__dirname + "/CeacarsCipher.html")
+});
+
+app.get('/Rsa', (req, res) => {
+    res.sendFile(__dirname + "/Rsa.html")
+});
+
+app.get('/RailFenceCipher', (req, res) => {
+    res.sendFile(__dirname + "/RailFenceCipher.html")
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
 });
